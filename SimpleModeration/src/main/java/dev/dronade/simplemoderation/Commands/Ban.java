cafc0a592ce48cd36f9915653543e0e4f820c86d
Emily Canto <56757680+dronade@users.noreply.github.com>
2022-02-17 16:28:47 +0000

    it works!!!!!!
    ban is now working as intended 😎

--- SimpleModeration/src/main/java/dev/dronade/simplemoderation/Commands/Ban.java
+++ SimpleModeration/src/main/java/dev/dronade/simplemoderation/Commands/Ban.java
@@ -24,7 +24,6 @@ public class Ban extends ModerationCommand implements CommandExecutor {
         //to do:
         // abstract out player permissions, argument length check, and player exists check (moderationCommand)
         // add pardon command to reverse this
-        // condition for a duration but no reason
         // be able to perform command from terminal
 
         if (commandSender instanceof Player) {
@@ -36,15 +35,7 @@ public class Ban extends ModerationCommand implements CommandExecutor {
                 player.sendMessage(Colours.colors("&4You are not permitted to use this command."));
                 return false;
             }
-
-            if (args.length > 3){
-                player.sendMessage(Colours.colors("&4Please use in format '/ban <username> [duration] [reason]'."));
-                return false;
-            }
-            // arg 0: username
-            // arg 1: duration/reason
-            // arg 2: reason
-
+            // ** next thing to sort out is this block **
             Player targetPlayer = Bukkit.getPlayer(args[0]);
             if (targetPlayer == null){
                 targetPlayer = (Player) Bukkit.getServer().getOfflinePlayer(args[0]);
@@ -53,15 +44,26 @@ public class Ban extends ModerationCommand implements CommandExecutor {
                 }
                 return false;
             }
-            // might be better off in a switch statement?
-            // case: no duration or reason
-            if (args.length == 1){
+
+            // case: duration AND reason
+            if (args.length == 3){
+                int duration;
+                try {
+                    duration = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    player.sendMessage(Colours.colors("&4 Please enter a valid number of hours!"));
+                    return false;
+                }
+                String reason = args[2];
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.HOUR, duration);
                 Bukkit.getBanList(BanList.Type.NAME).addBan(targetPlayer.getName(),
-                        Colours.colors("&4&l Banned" ), null, null);
+                        Colours.colors("&4" + reason ), calendar.getTime(), null);
                 boolean isOnline = targetPlayer.isOnline();
                 if (isOnline = true){
-                    targetPlayer.kickPlayer(Colours.colors("&4&o You have been banned by " + player.getName()));
+                    targetPlayer.kickPlayer(Colours.colors("&4&o You have been banned by " + player.getName() + "&4&o for " + reason));
                 }
+
             } else if (args.length == 2){
                 String reason = null;
                 int duration = 0;
@@ -75,6 +77,7 @@ public class Ban extends ModerationCommand implements CommandExecutor {
                     Calendar calendar = Calendar.getInstance();
                     calendar.add(Calendar.HOUR, duration);
                     Bukkit.getBanList(BanList.Type.NAME).addBan(targetPlayer.getName(),
+                            //need to provide default reason?
                             Colours.colors("&4&l Banned" ), calendar.getTime(), null);
                     boolean isOnline = targetPlayer.isOnline();
                     if (isOnline = true){
@@ -89,26 +92,16 @@ public class Ban extends ModerationCommand implements CommandExecutor {
                         targetPlayer.kickPlayer(Colours.colors("&4&o You have been kicked by " + player.getName() + "&4&l for " + reason));
                     }
                 }
-                // case: duration AND reason
-            } else if (args.length == 3){
-                int duration;
-                try {
-                    duration = Integer.parseInt(args[2]);
-                } catch (NumberFormatException e) {
-                    player.sendMessage(Colours.colors("&4 Please enter a valid number of hours!"));
-                    return false;
-                }
-                String reason = args[3];
-                Calendar calendar = Calendar.getInstance();
-                calendar.add(Calendar.HOUR, duration);
+                // case: no duration or reason
+            } else if (args.length == 1){
                 Bukkit.getBanList(BanList.Type.NAME).addBan(targetPlayer.getName(),
-                        Colours.colors("&4&l" + reason ), calendar.getTime(), null);
+                        Colours.colors("&4&l Banned" ), null, null);
                 boolean isOnline = targetPlayer.isOnline();
                 if (isOnline = true){
-                    targetPlayer.kickPlayer(Colours.colors("&4&o You have been banned by " + player.getName() + "&4&l for " + reason));
+                    targetPlayer.kickPlayer(Colours.colors("&4&o You have been banned by " + player.getName()));
                 }
             } else {
-                player.sendMessage(Colours.colors("&4 Too many arguments!"));
+                player.sendMessage(Colours.colors("&4Please use in format '/ban <username> [duration] [reason]'."));
             }
 
         }
